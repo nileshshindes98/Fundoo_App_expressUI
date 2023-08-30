4cc329d809c1bf32c37e6d61c05c8bb53ded914e
Nilesh Shinde <nileshshindes98@gmail.com>
2023-08-30 23:39:54 +0530

    add validator for new user registration

--- src/validators/user.validator.js
+++ src/validators/user.validator.js
@@ -1,14 +1,44 @@
 import Joi from '@hapi/joi';
-
+import HttpStatus from 'http-status-codes';
 export const newUserValidator = (req, res, next) => {
   const schema = Joi.object({
-    name: Joi.string().min(4).required()
+    name: Joi.string()
+      .min(4)
+      .required()
+      .pattern(/^[A-Z]+[a-zA-Z]{2,}[0-9]*$/),
+
+    lastName: Joi.string()
+      .min(4)
+      .required()
+      .pattern(/^[A-Z]+[a-zA-Z]{2,}[0-9]*$/),
+
+    email: Joi.string()
+      .required()
+      .pattern(/^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/),
+
+    password: Joi.string()
+      .min(8)
+      .required()
+      .pattern(
+        /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z0-9!@#$%^&*()_+{}|:;<>,.?/-]).{8,}$/
+      )
   });
   const { error, value } = schema.validate(req.body);
   if (error) {
-    next(error);
+    res.status(HttpStatus.NOT_ACCEPTABLE).json({
+      code: HttpStatus.NOT_ACCEPTABLE,
+      message: `${error}`
+    });
   } else {
-    req.validatedBody = value;
     next();
+  }
+};
+
+export const validateLogin = (user, password) => {
+  if (!user || user.password !== password) {
+    throw {
+      code: HttpStatus.UNAUTHORIZED,
+      message: 'Invalid email or password'
+    };
   }
 };
